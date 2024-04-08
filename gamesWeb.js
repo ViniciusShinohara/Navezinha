f919926ff12e341df23f3c173ce961bd323d5dac
ViniciusShinohara <160783723+ViniciusShinohara@users.noreply.github.com>
2024-04-08 19:18:29 +0000

    p1

--- gamesWeb.js
+++ gamesWeb.js
@@ -1,10 +1,9 @@
 function desenhaImagens(){
-
-canvas = document.getElementById("canvas")
+canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 img = new Image();
-Img.src = "";
-Img.onload = function(){
+img.src = "imagens/navealiada.jpg";
+img.onload = function(){
     ctx.drawImage(img, 0, 0);
     ctx.drawImage(img, 600, 0, 100, 100);
 }
